9f93112aa25eb07972aca28819c9af2239b2ff06
seri <seriputera2@gmail.com>
2022-01-02 19:51:58 +0800

    .

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
 const express = require("express");
 const path = require("path");
 const youtubedl = require("youtube-dl-exec");
+const bodyParser = require("body-parser");
+const serveIndex = require("serve-index");
 
 const app = express();
 const port = process.env.PORT || 5002;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/index.html"));
 });
 
+app.use("/downloads", express.static("/downloads"));
+app.use("/downloads", serveIndex(__dirname + "/downloads"));
+
 app.post("/add", function (req, res) {
     youtubedl(req.body.url, {
-        dumpSingleJson: true,
+        // dumpSingleJson: true,
         noWarnings: true,
         noCallHome: true,
         noCheckCertificate: true,
         youtubeSkipDashManifest: true,
         extractAudio: true,
         audioFormat: "mp3",
-        output: "%(title)s.%(ext)s",
-    }).then((output) => {
-        res.redirect(output.url);
-    });
+        output: "/downloads/%(title)s.%(ext)s",
+    })
+        .then((output) => {
+            console.log(output);
+            res.redirect("/downloads");
+        })
+        .catch((error) => {
+            console.log(error);
+            res.send(error);
+        });
 });
 
 app.listen(port);
